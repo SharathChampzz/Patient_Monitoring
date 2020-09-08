ef3ae7ce1e482ed3c0fc6de9219aef9b3084ed84
Sharath Kumar H K <54370770+SharathChampzz@users.noreply.github.com>
2020-09-08 12:54:29 +0530

    Update Monitor.py

--- Monitor.py
+++ Monitor.py
@@ -3,9 +3,9 @@ from PatientMonitor.PyToFireBase import update
 from time import sleep
 print('Imported Functions Successfully..!!')
 
-
-ip1 = '10.24.63.48'  #input('Enter Patient - 1 IP adress : ')
-ip2 = '10.24.61.74'  #input('Enter Patient - 2 IP adress : ')
+# Static IP should be used to nodemcu's
+ip1 = '10.24.63.48'  # input('Enter Patient - 1 IP adress : ')
+ip2 = '10.24.61.74'  # input('Enter Patient - 2 IP adress : ')
 url1 = 'http://' + ip1 + '/'
 url2 = 'http://' + ip2 + '/'
 
@@ -13,9 +13,9 @@ while True:
     pt1 = getData(url1)
     pt2 = getData(url2)
     print(f'Patient 1 : {pt1}')
-    update(pt1)
+#     update(pt1)
     print(f'Patient 2 : {pt2}')
-    update(pt1, pt2)
+    update(pt1, pt2) # To update more sensor readings increase parameters and also make suitable changes in imported file
     sleep(1)
     
 
